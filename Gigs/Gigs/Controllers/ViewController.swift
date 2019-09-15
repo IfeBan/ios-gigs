4895e1a9c0591c561c0108cafb5d97cb0f36f303
Ife Banire <Ifebanire@yahoo.com>
2019-09-15 19:15:59 -0400

    Created the User Interface

---
+++ Gigs/Gigs/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gigs
+//
+//  Created by Lambda_School_Loaner_101 on 9/12/19.
+//  Copyright © 2019 Ife Banire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
